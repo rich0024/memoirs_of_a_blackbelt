1d96488dd9fd7b185a1257fa4651ead132c5a592
Richard Yance <yance0022@gmail.com>
2019-05-30 01:53:16 +0000

    edit & delete3

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
     has_secure_password
     validates :username, presence: true
     validates :username, uniqueness: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
 
     has_many :moves
 end
